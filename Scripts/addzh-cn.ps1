f049e5b80ff40b79fa21d64d8c76a9ea19e18fe5
Chuyang Chen <chuyangchen2018@outlook.com>
2024-03-27 07:54:12 +0800

    Add optional support for Double Pinyin

--- Scripts/addzh-cn.ps1
+++ Scripts/addzh-cn.ps1
@@ -3,3 +3,7 @@ $LanguageList = Get-WinUserLanguageList
 $LanguageList.Add("zh-Hans-CN")
 
 Set-WinUserLanguageList $LanguageList -Force
+
+# New-ItemProperty -Path HKCU:Software\Microsoft\InputMethod\Settings\CHS -Name "Enable Double Pinyin" -Value 1 -Force -PropertyType DWord
+# New-ItemProperty -Path HKCU:Software\Microsoft\InputMethod\Settings\CHS -Name "UserDefinedDoublePinyinScheme0" -Value 'Xiaohe Code*2*^*iuvdjhcwfg^xmlnpbksqszxkrltvyovt' -PropertyType String -Force
+# New-ItemProperty -Path HKCU:Software\Microsoft\InputMethod\Settings\CHS -Name "DoublePinyinScheme" -Value 10 -Force -PropertyType DWord
